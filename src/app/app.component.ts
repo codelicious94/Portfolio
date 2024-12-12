0ace03a1701f0d76d7a3c00cf6646c185dd07260
codelicious94 <simi.matter@gmail.com>
2024-12-12 20:59:02 +0100

    update portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { HeroComponent } from './hero/hero.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, HeroComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, HeroComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
