22c9cb7d7d6cf79e9b5b1d3eb8a227374bd45d55
codelicious94 <simi.matter@gmail.com>
2025-01-05 18:25:51 +0100

    update scrollIntoView

--- src/app/main-content/navbar/navbar.component.ts
+++ src/app/main-content/navbar/navbar.component.ts
@@ -27,78 +27,14 @@ export class NavbarComponent {
     document.body.style.overflow = 'auto'; 
   }
 
-  scrollToPosition(positionY: number) {
-    const screenWidth = window.innerWidth;
-    const adjustedPosition = this.getAdjustedPosition(screenWidth, positionY);
-  
-    window.scrollTo({
-      top: adjustedPosition,
-      behavior: 'smooth'
-    });
-  }
-  
-  getAdjustedPosition(screenWidth: number, positionY: number): number {
-    if (screenWidth >= 1200 && screenWidth < 1350) {
-      return this.getPositionForLargeScreens(positionY);
-    } else if (screenWidth < 1200 && screenWidth > 650) {
-      return this.getPositionForMediumScreens(positionY);
-    } else if (screenWidth <= 650 && screenWidth > 450) {
-      return this.getPositionForSmallScreens(positionY);
-    } else if (screenWidth <= 450) {
-      return this.getPositionForExtraSmallScreens(positionY);
+  navigateToSection(event: Event) {
+    event.preventDefault(); 
+    const target = (event.target as HTMLAnchorElement).getAttribute('href');
+    if (target) {
+      const section = document.querySelector(target);
+      if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+      }
     }
-    return positionY; 
-  }
-  
-  getPositionForLargeScreens(positionY: number): number {
-    switch (positionY) {
-      case 800:
-        return 1200;
-      case 1700:
-        return 2100;
-      case 2500:
-        return 2900;
-      default:
-        return positionY;
-    }
-  }
-  
-  getPositionForMediumScreens(positionY: number): number {
-    switch (positionY) {
-      case 800:
-        return 1200;
-      case 1700:
-        return 2000;
-      case 2500:
-        return 3300;
-      default:
-        return positionY;
-    }
-  }
-  
-  getPositionForSmallScreens(positionY: number): number {
-    switch (positionY) {
-      case 800:
-        return 1000;
-      case 1700:
-        return 1700;
-      case 2500:
-        return 3100;
-      default:
-        return positionY;
-    }
-  }
-  
-  getPositionForExtraSmallScreens(positionY: number): number {
-    switch (positionY) {
-      case 800:
-        return 750;
-      case 1700:
-        return 1600;
-      case 2500:
-        return 2950;
-      default:
-        return positionY;
-    }
-  }
+  }  
 }
